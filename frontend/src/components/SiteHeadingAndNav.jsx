f135f9797e54d505361ba6b57ab0030498bad8b7
MtLight <tkory85@gmail.com>
2024-01-17 06:56:39 +0900

    v1.0.0 Major UI Changes and backend stuff

--- frontend/src/components/SiteHeadingAndNav.jsx
+++ frontend/src/components/SiteHeadingAndNav.jsx
@@ -1,16 +1,24 @@
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import CurrentUserContext from "../contexts/current-user-context";
+import './Navbar.css'
 
-export default function SiteHeadingAndNav() {
+export default function SiteHeadingAndNav({setSearchTerm}) {
   const { currentUser } = useContext(CurrentUserContext);
 
+  const handleChange = e => {
+    e.preventDefault();
+    setSearchTerm(e.target.elements.searchWords.value);
+    console.log("submit", e.target.elements.searchWords.value)
+  };
+
   return <header>
-    <a id='logo' href='/'>React/Express Auth</a>
+    <a id='logo' href='/'>Anime List</a>
     <nav>
       <ul>
         <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/users' end={true}>Users</NavLink></li>
+     
         {
           currentUser
             ? <li><NavLink to={`/users/${currentUser.id}`}>{currentUser.username}</NavLink></li>
@@ -19,6 +27,14 @@ export default function SiteHeadingAndNav() {
               <li><NavLink to='/sign-up'>Sign Up</NavLink></li>
             </>
         }
+        <form onSubmit={handleChange}>
+          <input 
+            name="searchWords"  
+            className="search" 
+            type="search" 
+            placeholder="Search.."
+          />
+        </form>
       </ul>
     </nav>
   </header>;
