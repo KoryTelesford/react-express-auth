d0102a761dc4537aabd59282609050dc34e830c8
Joeyyyys <josephgiler2@gmail.com>
2024-01-17 16:58:37 -0500

    ran a specific code that only runs the seed file we want to insert some data to have our properties in the database, also made a anime class with instance methods to be able to then use it inside our middleware, and through the request we are able to use these instance methods in the express router method

--- src/db/seeds/init.js
+++ src/db/seeds/init.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
--- src/middleware/add-models-to-request.js
+++ src/middleware/add-models-to-request.js
@@ -1,8 +1,10 @@
 const User = require('../db/models/user');
+const Anime = require('../db/models/anime');
 
 const addModelsToRequest = (req, res, next) => {
   req.db = {
     User,
+    Anime, // we are adding our Anime instance methods into the request body, making it accessible to route handlers
   };
   next();
 };
