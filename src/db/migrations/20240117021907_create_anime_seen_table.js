963ce5d658d0a67b6e2652491ac68aaa2b781f13
MtLight <tkory85@gmail.com>
2024-01-19 02:26:14 +0900

    Getting same backend

--- src/db/migrations/20240117021907_create_anime_seen_table.js
+++ src/db/migrations/20240117021907_create_anime_seen_table.js
@@ -5,7 +5,7 @@
 exports.up = (knex) => knex.schema.createTable('anime_seen', (table) => { // for the watchlist table, that the user has seen
     table.increments();  // makes the id sequence for our table
     table.integer('anime_id') // this is the anime id from our anime API that we are saving
-    table.integer('user_id').references('id').inTable('users').notNullable(); // creates a foreign key referencing our id property in the user table
+    table.integer('user_id').references('id').inTable('users').notNullable().onDelete('CASCADE'); // creates a foreign key referencing our id property in the user table
     table.timestamp('date_added').defaultTo(knex.fn.now());  // when the anime was added to the user's watch list
 
 });
