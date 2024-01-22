5921e1b9e4b54f637bd7b3779eab0162941ccb0d
Joeyyyys <josephgiler2@gmail.com>
2024-01-22 13:35:50 -0500

    made a static function that updates an anime_id by using another static function that finds by the id, animeid, and userid, from there the user can input a new animeid, which would then update that specific row of the id sequence to only update (using patch http verb) so that it wont update all of the user's anime, and the user would want to update the anime id in case they want to choose another anime

--- src/controllers/anime/update.js
+++ src/controllers/anime/update.js
@@ -1,16 +1,16 @@
 const updateAnime = async (req, res) => {
     const {
         db: { Anime },
-        body: { new_anime_id }, // the parameter put by the user
-        params: { anime_id, user_id },    // grabbing the actual IDs from the db
-    } = req
+        body: {new_anime_id},
+        params: {id, user_id, anime_id}
+    } = req;
 
-    const anime = await Anime.find(anime_id, user_id);
-    if (!anime) return res.sendStatus(404);
+    console.log("ihff", id, anime_id, user_id )
+    const animeExists = await Anime.findById(id, anime_id, user_id)
+    if (!animeExists)  return res.sendStatus(404);
 
-    const updatedAnime = await Anime.update(new_anime_id);
-    res.send(updatedAnime);
-
-};
+    const updatedAnimeId = await Anime.update(new_anime_id, id, user_id, anime_id);
+    res.send(updatedAnimeId);
+}
 
 module.exports = updateAnime;
--- src/db/models/anime.js
+++ src/db/models/anime.js
@@ -11,11 +11,6 @@ class Anime {
         this.date_added = date_added;
     }
 
-    // static async list(user_id) {
-    //     const query = 'SELECT * FROM anime_seen WHERE user_id = ?';
-    //     const response = await knex.raw(query, [user_id]);
-    //     return response.rows || [];
-    // }
 
     static async list(user_id) {
         console.log('Querying animes for user ID:', user_id);
@@ -32,19 +27,23 @@ class Anime {
         const anime = rows[0];
         return new Anime(anime);
     }
-
-    static async update(new_anime_id) {
-        const query = 'UPDATE anime_seen SET anime_id = ? WHERE user_id = ?';
-        const args = [new_anime_id];
-        const { rows } = await knex.raw(query, args)
-        const updatedAnime = rows[0];
-        return new Anime(updatedAnime);
-    }
-
+    
     static async delete(anime_id, user_id){
         const query = `DELETE FROM anime_seen WHERE anime_id = ? AND user_id = ? RETURNING *`;
         const { rows: [removedAnime] } = await knex.raw(query, [anime_id, user_id]);
         return removedAnime ? new Anime(removedAnime) : null;      // this is so that we can see that we deleted, an instance of that
+    }
+
+    static async update(new_anime_id, id, user_id, anime_id){
+        const query = 'UPDATE anime_seen SET anime_id = ? WHERE id = ? AND user_id = ? AND anime_id = ? RETURNING *';
+        const args = [new_anime_id, id, user_id, anime_id]
+        console.log('Executing query:', query);
+        console.log('Query arguments:', args);
+
+        const { rows } = await knex.raw(query, args);
+        console.log('Query result:', rows);
+
+        return rows[0] || null;
     }
 
     static async find(anime_id, user_id){
@@ -53,6 +52,14 @@ class Anime {
         const { rows } = await knex.raw(query, args);
         const anime = rows[0];
         return anime ? new Anime(anime) : null;
+    }
+
+    static async findById(id, anime_id, user_id){ 
+        const query = 'SELECT * FROM anime_seen WHERE id = ? AND anime_id = ? AND user_id = ?';
+        const args = [id, anime_id, user_id];
+        const { rows } = await knex.raw(query, args);
+        const animeRowId = rows[0];
+        return animeRowId ? new Anime(animeRowId) : null
     }
 };
 
